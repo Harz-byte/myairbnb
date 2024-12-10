782dfa89ed88944da9c686e771976e1c90ede774
Harzron <rohangowda2913@gmail.com>
2024-12-10 19:48:18 +0530

    2nd commit

---
+++ src/main/java/com/reservationapp/controller/PaymentController.java
@@ -0,0 +1,10 @@
+package com.reservationapp.controller;
+
+public class PaymentController {
+
+    private String  paymentType;
+
+    public PaymentController(String paymentType){
+        this.paymentType = paymentType;
+    }
+}
